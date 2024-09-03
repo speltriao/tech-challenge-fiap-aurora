46549e6bb2afc176e62799d97edd2d6db2c6064c
Alexandre Speltri <alexandreporks@gmail.com>
2024-09-02 21:06:24 -0300

    Versão simplificada inicial

--- main.tf
+++ main.tf
@@ -2,29 +2,22 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_rds_cluster" "aurora_cluster" {
-  cluster_identifier      = "aurora-cluster"
-  engine                  = "aurora-mysql"
-  engine_version          = "5.7.mysql_aurora.2.03.4"
-  master_username         = var.db_username
-  master_password         = var.db_password
-  database_name           = var.db_name
-  backup_retention_period = 5
-  preferred_backup_window = "07:00-09:00"
-  skip_final_snapshot     = true
-
-  vpc_security_group_ids  = var.vpc_security_group_ids
-  db_subnet_group_name    = aws_db_subnet_group.main.name
+resource "aws_rds_cluster" "aurora_postgres" {
+  engine             = "aurora-postgresql"
+  engine_version     = "14.6"
+  cluster_identifier = "my-aurora-cluster"
+  master_username    = "postgres"
+  master_password    = "postgres"
+  skip_final_snapshot = true
 }
 
-resource "aws_db_subnet_group" "main" {
-  name       = "aurora-subnet-group"
-  subnet_ids = var.db_subnet_ids
+resource "aws_rds_cluster_instance" "aurora_postgres_instance" {
+  identifier        = "aurora-instance-1"
+  cluster_identifier = aws_rds_cluster.aurora_postgres.id
+  instance_class    = "db.t4g.medium"
+  engine            = aws_rds_cluster.aurora_postgres.engine
 }
 
-resource "aws_rds_cluster_instance" "aurora_instance" {
-  count              = 2
-  identifier         = "aurora-instance-${count.index + 1}"
-  cluster_identifier = aws_rds_cluster.aurora_cluster.id
-  instance_class     = var.db_instance_class
+output "endpoint" {
+  value = aws_rds_cluster.aurora_postgres.endpoint
 }
--- outputs.tf
+++
@@ -1,3 +0,0 @@
-output "aurora_cluster_endpoint" {
-  value = aws_rds_cluster.aurora_cluster.endpoint
-}
