0c1497b9da6b3d88065fc4ac4b6cfff4b83f2ba3
Alexandre Speltri <alexandrespeltri@gmail.com>
2024-09-11 23:39:59 -0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,6 +2,10 @@ provider "aws" {
   region = "us-east-1"
 }
 
+data "aws_security_group" "existing_sg" {
+  id = "sg-02de03c61df5358f3"
+}
+
 # Define the DB subnet group with the public subnet
 resource "aws_db_subnet_group" "aurora_subnet_group" {
   name        = "aurora-subnet-group"
@@ -18,21 +22,22 @@ resource "aws_db_subnet_group" "aurora_subnet_group" {
 resource "aws_rds_cluster" "aurora_postgres" {
   engine             = "aurora-postgresql"
   engine_version     = "14.6"
-  cluster_identifier = "galega-db-aurora"
+  cluster_identifier = "galega-db-aurora1"
   master_username    = var.db_master_username
   master_password    = var.db_master_password
   skip_final_snapshot = true
   db_subnet_group_name = aws_db_subnet_group.aurora_subnet_group.name  # Link the subnet group
+  vpc_security_group_ids = [data.aws_security_group.existing_sg.id]  # Reference to the existing security group
 
   # Optional tags for resource identification
   tags = {
-    Name = "galega-db-aurora"
+    Name = "galega-db-aurora1"
   }
 }
 
 # Define the RDS cluster instance
 resource "aws_rds_cluster_instance" "aurora_postgres_instance" {
-  identifier           = "aurora-db-instance-1"
+  identifier           = "aurora-db-instance-2"
   cluster_identifier   = aws_rds_cluster.aurora_postgres.id
   instance_class       = "db.t4g.medium"
   engine               = aws_rds_cluster.aurora_postgres.engine
